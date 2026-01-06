4410eff5a6d41bf7a75dabff8cc145f3cd56d719
Hazam Liaqat <bscsf23m01@gmail.com>
2026-01-06 19:19:00 +0500

    Add files via upload

---
+++ hehe.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+
+    cout << "Enter how many numbers you want to input: ";
+    cin >> n;
+
+    cout << "Enter " << n << " numbers:\n";
+    for (int i = 0; i < n; i++) {
+        int num;
+        cin >> num;
+        if (num % 2 == 0) {
+            cout << num << " is even.\n";
+        }
+    }
+
+    return 0;
+}
