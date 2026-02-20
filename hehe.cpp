574aabc08209acc15759b1b68f0652dd270042de
Hazam Liaqat <bscsf23m01@gmail.com>
2026-02-20 11:52:19 +0500

    Update hehe.cpp

--- hehe.cpp
+++ hehe.cpp
@@ -4,7 +4,7 @@ using namespace std;
 int main() {
     int n;
 
-    cout << "Enter how many number you want to input: ";
+    cout << "Enter how many numbers you want to input: ";
     cin >> n;
 
     cout << "Enter " << n << " numbers:\n";
